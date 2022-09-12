908f109c6fd93661dd9457ddb36e7b7b0b8a0eb2
eugcapt <zhk.sem@yandex.ru>
2022-09-12 22:18:27 +0300

    button now working using data binding

--- app/src/main/java/com/eugcapt/workoutactivity/MainActivity.kt
+++ app/src/main/java/com/eugcapt/workoutactivity/MainActivity.kt
@@ -2,12 +2,24 @@ package com.eugcapt.workoutactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.eugcapt.workoutactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit  var  activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        activityMainBinding.button.setOnClickListener { changeText() }
+
+    }
+
+    private fun changeText() {
+        activityMainBinding.textView.setText("Data Binding")
     }
 }
